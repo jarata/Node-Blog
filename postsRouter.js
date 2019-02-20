9c2b5134604456acbf8ca319d1c4eb969842fd2e
J A <jarata@gmail.com>
2019-02-20 14:06:47 -0800

    posts/:id GET

--- postsRouter.js
+++ postsRouter.js
@@ -14,6 +14,28 @@ router.get('/', async (req, res) => {
         })
     }
 });
+// function getById(id) {
+//   return db('posts')
+//     .where({ id })
+//     .first();
+// }
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.getById(req.params.id);
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "The post information could not be retrieved.",
+        });
+    }
+});
 
 module.exports = router;
 
@@ -21,11 +43,7 @@ module.exports = router;
 //   return db('posts');
 // }
 //
-// function getById(id) {
-//   return db('posts')
-//     .where({ id })
-//     .first();
-// }
+
 //
 // function insert(post) {
 //   return db('posts')
