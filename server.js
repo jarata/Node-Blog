65ff329ea92712a5196ef9a622ce1bdad4f2b0cc
J A <jarata@gmail.com>
2019-02-20 15:40:26 -0800

    imported helmet and morgan middleWare

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 const postsRouter = require('./postsRouter');
 const usersRouter = require('./usersRouter');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
 server.use('/api/posts', postsRouter);
 server.use('/api/users', usersRouter);
 
