ea1ced7599336b64243d8f79d915f32624c85544
J A <jarata@gmail.com>
2019-02-20 14:49:24 -0800

    users/:id GET

--- usersRouter.js
+++ usersRouter.js
@@ -26,6 +26,24 @@ router.get('/', async (req, res) => {
 //         .first();
 // }
 
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await Users.getById(req.params.id);
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist."
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "The user information could not be retrieved.",
+        });
+    }
+});
+
 // router.get('/:id', async (req, res) => {
 //
 // })
