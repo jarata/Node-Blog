93933cd13cc4c4856cb4c8985134f8364391b74d
J A <jarata@gmail.com>
2019-02-20 14:47:25 -0800

    users GET

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const postsRouter = require('./postsRouter');
-// const usersRouter = require('./usersRouter');
+const usersRouter = require('./usersRouter');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/posts', postsRouter);
-// server.use('/api/users', usersRouter);
+server.use('/api/users', usersRouter);
 
 server.get('/', async  (req, res) => {
     res.send(`<h2>Server is online!</h2>`)
--- usersRouter.js
+++ usersRouter.js
@@ -3,8 +3,58 @@ const Users = require('./data/helpers/userDb');
 
 const router = express.Router();
 
-router.get('/'. async (req, res) => {
-    try {
 
+//function get() {
+//     return db('users');
+// }
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.get();
+        res.status(200).json(users)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "The users information could not be retrieved."
+        })
     }
-})
+});
+
+// function getById(id) {
+//     return db('users')
+//         .where({ id })
+//         .first();
+// }
+
+// router.get('/:id', async (req, res) => {
+//
+// })
+
+// function getUserPosts(userId) {
+//     return db('posts as p')
+//         .join('users as u', 'u.id', 'p.user_id')
+//         .select('p.id', 'p.text', 'u.name as postedBy')
+//         .where('p.user_id', userId);
+// }
+
+// function insert(user) {
+//     return db('users')
+//         .insert(user)
+//         .then(ids => {
+//             return getById(ids[0]);
+//         });
+// }
+
+// function update(id, changes) {
+//     return db('users')
+//         .where({ id })
+//         .update(changes);
+// }
+
+// function remove(id) {
+//     return db('users')
+//         .where('id', id)
+//         .del();
+// }
+
+module.exports = router;
