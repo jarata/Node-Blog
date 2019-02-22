9bcedd9197be3b7d0151eb7164222cff1e0f0a5d
J A <jarata@gmail.com>
2019-02-22 13:49:28 -0800

    added cors to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const helmet = require('helmet');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const postsRouter = require('./postsRouter');
 const usersRouter = require('./usersRouter');
@@ -11,6 +12,7 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
+server.use(cors());
 server.use('/api/posts', postsRouter);
 server.use('/api/users', usersRouter);
 
